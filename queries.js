d1b0b02f5a5b158db1a09c26eb6f8a6d3e3ec62c
Anton Karatkevich <zhl96@mail.ru>
2019-08-28 14:53:12 +0300

    feat: add auth

--- queries.js
+++ queries.js
@@ -7,28 +7,61 @@ const pool = new Pool({
 });
 const crypto = require('crypto');
 const util = require('util');
+const jwt = require('jsonwebtoken');
+const passport = require('passport');
+const passportJWT = require('passport-jwt');
+
+const ExtractJwt = passportJWT.ExtractJwt;
+const JwtStrategy = passportJWT.Strategy;
+
+const options = {
+	secretOrKey: process.env.DB_PUBLIC_KEY,
+	jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+};
+
+passport.use(new JwtStrategy(options, (jwt_payload, done) => {
+	pool.query('SELECT id, role FROM users WHERE id = $1', [jwt_payload.id], (err, result) => {
+		const isUserHasToken = result.rows.some(item => item.id === jwt_payload.id);
+		if (isUserHasToken) {
+			done(null, { authUserId: jwt_payload.id, role: result.rows[0].role });
+		} else {
+			done(null);
+		}
+	});
+}));
 
 const getUsers = (req, res) => {
-	pool.query('SELECT id, firstname, lastname FROM users', (err, result) => {
+	const { authUserId } = req.user;
+	pool.query('SELECT role FROM users WHERE id = $1', [authUserId], (err, result) => {
 		if (err) {
 			throw new Error(err.message);
 		}
-		res.status(200).json(result.rows);
+		pool.query('SELECT id, firstname, lastname FROM users', (err, result) => {
+			if (err) {
+				throw new Error(err.message);
+			}
+			res.send(result.rows)
+		});
 	});
 };
 
 const getUserById = (req, res) => {
 	const id = parseInt(req.params.id);
-
-	pool.query(
-		'SELECT * FROM users WHERE id = $1',
-		[id],
-		(err, result) => {
-			if (err) {
-				throw new Error(err.message);
-			}
-			res.status(200).json(result.rows);
-		});
+	const { authUserId } = req.user;
+	pool.query('SELECT role FROM users WHERE id = $1', [authUserId], (err, result) => {
+		if (err) {
+			throw new Error(err.message);
+		}
+		pool.query(
+			'SELECT id, firstname, lastname, email, description FROM users WHERE id = $1', //dont take all info
+			[id],
+			(err, result) => {
+				if (err) {
+					throw new Error(err.message);
+				}
+				res.status(200).json(result.rows);
+			});
+	});
 };
 
 const createUser = (req, res) => {
@@ -48,9 +81,8 @@ const createUser = (req, res) => {
 const updateUser = (req, res) => {
 	const id = parseInt(req.params.id);
 	const { firstname, lastname, email, description } = req.body;
-
 	pool.query(
-		'UPDATE profiles SET firstname = $1, lastname = $2, email = $3, description = $4 WHERE id = $5',
+		'UPDATE users SET firstname = $1, lastname = $2, email = $3, description = $4 WHERE id = $5',
 		[firstname, lastname, email, description, id],
 		(err, result) => {
 			if (err) {
@@ -65,7 +97,7 @@ const deleteUser = (req, res) => {
 	const id = parseInt(req.body.id);
 
 	pool.query(
-		'DELETE FROM profiles WHERE id = $1',
+		'DELETE FROM users WHERE id = $1',
 		[id],
 		(err, result) => {
 			if (err) {
@@ -82,18 +114,51 @@ const registerUser = async (req, res) => {
 	const key = await getKey(password, salt, 5000, 8, 'sha512');
 	if (key) {
 		pool.query(
-			'INSERT INTO users (username, password, firstname, lastname) VALUES ($1, $2, $3, $4)',
-			[username, key.toString('hex'), firstname, lastname],
+			'INSERT INTO users (username, password, firstname, lastname, role) VALUES ($1, $2, $3, $4, $5)',
+			[username, key.toString('hex'), firstname, lastname, 'USER'],
 			(err, result) => {
 				if (err) {
 					throw new Error(err.message);
 				}
-				res.status(200).send(`User register succced`);
+				pool.query(
+					'SELECT id, role, firstname FROM users WHERE username = $1',
+					[username],
+					(err, result) => {
+						const token = jwt.sign({ id: result.rows[0].id }, process.env.DB_PUBLIC_KEY);
+						const { id, role, firstname } = result.rows[0];
+						res.status(200).send({ id: id, token: token, role: role, firstname: firstname });
+					}
+				)
 			});
 	} else {
 		console.error('eerrrooor');
 	}
-}
+};
+
+const loginUser = async (req, res) => {
+	const { username, password } = req.body;
+	const salt = process.env.DB_SALT;
+	const getKey = util.promisify(crypto.pbkdf2);
+	const key = await getKey(password, salt, 5000, 8, 'sha512');
+	if (key) {
+		pool.query(
+			'SELECT password, id, role, firstname FROM users WHERE username = $1',
+			[username],
+			(err, result) => {
+				if (err) {
+					throw new Error(err.message);
+				} else if (!result.rows.length) {
+					res.status(500).send(`User '${username}' is not exist`);
+				} else if (key.toString('hex') === result.rows[0].password) {
+					const token = jwt.sign({ id: result.rows[0].id }, process.env.DB_PUBLIC_KEY);
+					const { id, role, firstname } = result.rows[0];
+					res.status(200).send({ id: id, token: token, role: role, firstname: firstname });
+				}
+			});
+	} else {
+		console.error('eerrrooor');
+	}
+};
 
 module.exports = {
 	getUsers,
@@ -102,4 +167,5 @@ module.exports = {
 	updateUser,
 	deleteUser,
 	registerUser,
+	loginUser
 };
--- server.js
+++ server.js
@@ -4,6 +4,7 @@ require('dotenv').config();
 const app = express();
 const port = process.env.DB_PORT || 3001;
 const db = require('./queries');
+const passport = require('passport');
 
 app.use(bodyParser.json());
 app.use(
@@ -12,12 +13,35 @@ app.use(
     })
 );
 
-app.get('/api', db.getUsers);
-app.get('/user/:id', db.getUserById);
-app.post('/form', db.createUser);
-app.put('/edit/user/:id', db.updateUser);
-app.delete('/delete', db.deleteUser);
+const checkRoles = (roles) => {
+    return (req, res, next) => {
+        const { user } = req;
+        if (roles.includes(user.role)) {
+            next();
+        } else {
+            throw new Error('You are not administrator!!!');
+        }
+    }
+};
+
+const checkUserPermissions = () => (req, res, next) => {
+    const pageId = req.params.id;
+    const authId = req.user.id;
+    const role = req.user.role;
+    if (pageId === authId || role === 'ADMIN') {
+        next();
+    } else {
+        throw new Error('You are not administrator');
+    };
+};
+
+app.get('/api', passport.authenticate('jwt', { session: false }), checkRoles(['ADMIN', 'USER']), (req, res) => db.getUsers(req, res));
+app.get('/user/:id', passport.authenticate('jwt', { session: false }), checkUserPermissions(), (req, res) => db.getUserById(req, res));
+app.post('/form', passport.authenticate('jwt', { session: false }), checkRoles(['ADMIN']), (req, res) => db.createUser(req, res));
+app.put('/edit/user/:id', passport.authenticate('jwt', { session: false }), checkUserPermissions(), (req, res) => db.updateUser(req, res));
+app.delete('/delete', passport.authenticate('jwt', { session: false }), checkRoles(['ADMIN']), (req, res) => db.deleteUser(req, res));
 app.post('/register', db.registerUser);
+app.post('/login', db.loginUser);
 
 app.listen(port, () => {
     console.log(`Listenning on port ${port}`);
